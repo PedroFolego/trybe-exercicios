e32e1302a3adf86c98846fa4cead52e2a8608e7e
pedro <hmfpedro@hotmail.com>
2021-10-20 23:13:26 -0300

    a imagem nao entra nao sei pq

---
+++ fundamentos/bloco-5-DOM-evento-e-web-store/dia-2-trabalhando-com-elementos/script.js
@@ -0,0 +1,34 @@
+// arquivo script.js
+
+ // console.log(document.getElementById('start').nextSibling) // nó
+
+ // console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+
+
+/* document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'red';
+document.querySelector('#primeiroFilhoDoFilho').innerHTML = 'Você está prestando atenção';
+document.getElementById('pai').firstChild.innerHTML = 'Ola papai';
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling.innerHTML = 'Ola mundo';
+document.querySelector('#elementoOndeVoceEsta').parentNode.innerHTML = 'Atenção meu amigo';
+document.getElementById('elementoOndeVoceEsta').nextSibling.innerHTML = 'Ola Primo';
+console.log(document.getElementById('pai').children[2])
+ */
+
+// let elementOndeEstou = document.getElementById('elementoOndeVoceEsta')
+// let texto =  document.createElement('p');
+// texto.innerText = 'Ola Pedro';
+// elementOndeEstou.appendChild(texto);
+
+// let paiElemento = document.getElementById('pai');
+// let filhoDiv =  document.createElement('div');
+// filhoDiv.innerText = 'Ola Irmao';
+// paiElemento.appendChild(filhoDiv);
+
+// let filhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+// let headerFilho = document.createElement('header');
+// headerFilho.innerHTML = '<strong>Sou forte</strong>';
+// filhoDoFilho.appendChild(headerFilho);
+
+// filhoDoFilho.remove(headerFilho);
+// paiElemento.removeChild(filhoDiv);
+// elementOndeEstou.remove(texto);
