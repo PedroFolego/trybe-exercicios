265ea35b6fd968c80796e738bbf79871affd0b3b
pedro <hmfpedro@hotmail.com>
2021-11-15 00:31:29 -0300

    comecando bonus

--- fundamentos/bloco-7-introducao-JS-ES6-testes-unitarios/my_new_project/sum.test.js
+++ fundamentos/bloco-7-introducao-JS-ES6-testes-unitarios/my_new_project/sum.test.js
@@ -1,4 +1,4 @@
-const { sum, myRemove, myFizzBuzz, encode, decode } = require('./sum');
+const { sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate } = require('./sum');
 
 describe('testando funções', () => {
   it('testando soma da função (sum)', () => {
@@ -62,8 +62,6 @@ describe('Testando Encode e Decode', () => {
   })
 })
 
-const techList = require('./techList.js');
-
 describe('Testa a função techList', () => {
   it('Testa se a função techList é definida', () => {
     expect(techList).toBeDefined();
@@ -100,5 +98,19 @@ describe('Testa a função techList', () => {
   });
 });
 
-
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
+});
 
